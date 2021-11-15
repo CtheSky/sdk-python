a4d18484bd6a3a6cafcabc676d4bea1765239357
Hart Chu <cthesky13@gmail.com>
2021-11-15 13:52:59 +0800

    add get_order_states

--- pyinjective/async_client.py
+++ pyinjective/async_client.py
@@ -232,6 +232,10 @@ class AsyncClient:
         req = exchange_accounts_rpc_pb.SubaccountOrderSummaryRequest(subaccount_id=subaccount_id, order_direction=order_direction, market_id=market_id)
         return await self.stubExchangeAccount.SubaccountOrderSummary(req)
 
+    async def get_order_states(self, spot_order_hashes: list = '', derivative_order_hashes: list = ''):
+        req = exchange_accounts_rpc_pb.OrderStatesRequest(spot_order_hashes=spot_order_hashes, derivative_order_hashes=derivative_order_hashes)
+        return await self.stubExchangeAccount.OrderStates(req)
+
     # OracleRPC
 
     async def stream_oracle_prices(self, base_symbol: str, quote_symbol: str, oracle_type: str):
